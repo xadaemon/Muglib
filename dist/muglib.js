a2af3c0dd87559582b6436f979a05e8d7c8b6b47
matheusxaviersi <matheus_xavier-silva@live.com>
2014-10-23 22:49:03 -0200

    updated API to alpha 0.1.6 and library version 1.2.1

--- dist/muglib.js
+++ dist/muglib.js
@@ -4,7 +4,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 http://www.apache.org/licenses/LICENSE-2.0
-MuglibVersion 0.3.4 build at lab on day 10-14-2014
+MuglibVersion 1.2.1 build at lab on day 10-23-2014
 */
 (function ( window, undefined ) {
     var Mug = function (element, index) {
@@ -18,7 +18,45 @@ MuglibVersion 0.3.4 build at lab on day 10-14-2014
     Mug.fn = Mug.prototype;
     window.Mug = Mug;
     window.$ = Mug;
-})(window);Mug.fn = {
+})(window);Mug.fn.ajax = {
+    request: function(opts){
+        if (typeof opts == 'string') opts = {url:opts};
+        opts.url = opts.url || '';
+        opts.method = opts.method || 'get';
+        opts.data = opts.data || 'NO_DATA';
+        opts.async = opts.async || true;
+    },
+    api: {
+        r: new XMLHttpRequest(),
+        host: {},
+        setHeaders: function(opts){
+            for (var name in opts.headers) {
+                this.r && thi.r.setRequestHeader(name, opts.headers[name]);
+            };
+        },
+        req: function (opts) {
+            var self = this;
+            var r = new XMLHttpRequest();
+            this.r.onload = function(){
+                if (this.r.readyState == 4 && this.r.status == 200) {
+                    self.onSucess.apply(self.host, [this.r.responseText]);
+                }else{
+                    self.onSucess.apply(self.host, [this.r.statusText]);
+                }
+            }
+            if(opts.data != 'NO_DATA' && opts.method == 'get'){
+                this.r.open('GET', url, async);
+                this.r.send(data);
+            }else if(opts.data != 'NO_DATA' && opts.method == 'post'){
+                this.r.open('POST', url, async);
+                this.r.setHeaders({'Content-type':'application/x-www-form-urlencoded'});
+                this.r.send(data);
+            }
+            return this;
+            return api.req();
+        }
+    }
+};Mug.fn = {
     post: function (url, data, callback, async) {
         async = typeof async !== 'undefined' ? async : true;
         var r = new XMLHttpRequest();
---
+++ src/ajax/ajax.dev.js
@@ -0,0 +1,47 @@
+/*
+Copyright 2014 MugFoudation
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+*/
+//this is still a alpha version may not be reliable so use of th old api is still enabled
+Mug.fn.ajax = {
+    request: function(opts){
+        if (typeof opts == 'string') opts = {url:opts};
+        opts.url = opts.url || '';
+        opts.method = opts.method || 'get';
+        opts.data = opts.data || 'NO_DATA';
+        opts.async = opts.async || true;
+    },
+    api: {
+        r: new XMLHttpRequest(),
+        host: {},
+        setHeaders: function(opts){
+            for (var name in opts.headers) {
+                this.r && thi.r.setRequestHeader(name, opts.headers[name]);
+            };
+        },
+        req: function (opts) {
+            var self = this;
+            var r = new XMLHttpRequest();
+            this.r.onload = function(){
+                if (this.r.readyState == 4 && this.r.status == 200) {
+                    self.onSucess.apply(self.host, [this.r.responseText]);
+                }else{
+                    self.onSucess.apply(self.host, [this.r.statusText]);
+                }
+            }
+            if(opts.data != 'NO_DATA' && opts.method == 'get'){
+                this.r.open('GET', url, async);
+                this.r.send(data);
+            }else if(opts.data != 'NO_DATA' && opts.method == 'post'){
+                this.r.open('POST', url, async);
+                this.r.setHeaders({'Content-type':'application/x-www-form-urlencoded'});
+                this.r.send(data);
+            }
+            return this;
+            return api.req();
+        }
+    }
+};
